33dcc857496131436dae3e1a579844849ad92738
developernajib <najibivy1@gmail.com>
2022-12-06 22:51:42 +0600

    smart contract deployed

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ const index = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          bannerText="Buy & Sell extraordinary products with Web3.0"
+          bannerText="Buy & Sell unique products with Web3.0"
           parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
         />
