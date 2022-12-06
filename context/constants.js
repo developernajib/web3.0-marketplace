9d06c82573dc6b1ff0847f6354d1008bbd367c6e
developernajib <najibivy1@gmail.com>
2022-12-06 23:19:10 +0600

    context added

--- context/constants.js
+++ context/constants.js
@@ -1,7 +1,6 @@
+// deployed address: 0x5FbDB2315678afecb367f032d93F642f64180aa3
 /* eslint-disable quotes */
 import market from "./NFTMarketplace.json";
 
 export const MarketAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const MarketAddressABI = market.abi;
-
-// 0x5FbDB2315678afecb367f032d93F642f64180aa3
--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,27 @@
 import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 
+import { NFTProvider } from "../context/NFTContext";
 import { Navbar, Footer } from "../components";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script
-      src="https://kit.fontawesome.com/0c7e264496.js"
-      crossOrigin="anonymous"
-    />
-  </ThemeProvider>
+      <Script
+        src="https://kit.fontawesome.com/0c7e264496.js"
+        crossOrigin="anonymous"
+      />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default MyApp;
